906329c519e23707215a6fa5b9ab4935d9a2361b
Emma Finkel <emma.finkel@gmail.com>
2020-07-09 16:49:52 -0400

    Initial Commit

---
+++ Mini Project 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini Project 1
+//
+//  Created by Emma Finkel on 7/9/20.
+//  Copyright © 2020 Emma Finkel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
